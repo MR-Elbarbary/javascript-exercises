01ccfdf084038daec77c37219a2c5a92bdd54e60
MR-Elbarbary <121816435+MR-Elbarbary@users.noreply.github.com>
2024-07-30 21:01:48 +0300

    Assignment done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    items.forEach(item => {
+        while((index = array.indexOf(item)) !== -1){
+            array.splice(index, 1);
+        }
+    });
+    return array
 };
 
 // Do not edit below this line
